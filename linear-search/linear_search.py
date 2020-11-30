9248c6a01332548d741251e832eb636ceb6a7644
Думан <duman2k@gmail.com>
2020-11-30 20:13:48 +0600

    feat: solved liner_search

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,4 +1,11 @@
 def linear_search(lst, to_find):
-  # search for the element to_find inside lst
-  # if found, return index of element
-  # else return -1
+    index = 0
+    for item in lst:
+        if item == to_find:
+            return index
+        index += 1
+    return -1
+    
+    # search for the element to_find inside lst
+    # if found, return index of element
+    # else return -1
